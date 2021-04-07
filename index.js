1f24a7b8285a673ad30bd0f312bea141b3c4b63e
Manuel arcos <maalcas10jn@gmail.com>
2021-04-07 13:35:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app  = express();
+
+app.use(cors());
+// constante del puerto
+const port = process.env.PORT || 8080;
+//importar carpetas 
+const about =require("./JSON/About.json");
+const portafolio = require("./JSON/Portafolio.json");
+//rutas 
+app.get('/about', (req, res) =>{
+    res.json(about)
+});
+
+app.get('/portafolio', (req, res) =>{
+    res.json(portafolio)
+});
+
+//levantar el servidor 
+app.listen(port,() => {
+    console.log(`Servidor en el puerto ${port}`)
+});
